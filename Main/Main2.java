27030aef03153275a32eda6cccf087000cb6a5f8
EduardoOrozcoP <55148198+EduardoOrozcoP@users.noreply.github.com>
2019-10-01 23:02:20 -0500

    Add files via upload

---
+++ Main/Main2.java
@@ -0,0 +1,129 @@
+package Main;
+
+import Grafo.GraphM;
+
+public class Main2 {
+
+	public static void main(String[] args) {
+//	GraphM uno = new GraphM(30);
+//	uno.modeloER(29,false);
+//	uno.escribirArchivo("01ER030_2.gv");
+//	GraphM unoBFS = uno.BFS(0);
+//	unoBFS.escribirArchivo("01ER030_2BFS.gv");
+//	GraphM unoDFS = uno.DFS(0);
+//	unoDFS.escribirArchivo("01ER030_2DFS.gv");
+//	GraphM unoDFS_I = uno.DFS_I(0);
+//	unoDFS_I.escribirArchivo("01ER030_2DFS_I.gv");
+	
+//	GraphM dos = new GraphM(100);
+//	dos.modeloER(99,false);
+//	dos.escribirArchivo("02ER100_2.gv");
+//	GraphM dosBFS = dos.BFS(0);
+//	dosBFS.escribirArchivo("02ER100_2BFS.gv");
+//	GraphM dosDFS = dos.DFS(0);
+//	dosDFS.escribirArchivo("02ER100_2DFS.gv");
+//	GraphM dosDFS_I = dos.DFS_I(0);
+//	dosDFS_I.escribirArchivo("02ER100_2DFS_I.gv");
+	
+//	GraphM tres = new GraphM(500);
+//	tres.modeloER(499,false);
+//	tres.escribirArchivo("03ER500_2.gv");
+//	GraphM tresBFS = tres.BFS(0);
+//	tresBFS.escribirArchivo("03ER500_2BFS.gv");
+//	GraphM tresDFS = tres.DFS(0);
+//	tresDFS.escribirArchivo("03ER500_2DFS.gv");
+//	GraphM tresDFS_I = tres.DFS_I(0);
+//	tresDFS_I.escribirArchivo("03ER500_2DFS_I.gv");
+	
+//	GraphM cuatro = new GraphM(30);
+//	cuatro.modeloGilbert(0.9,false);
+//	cuatro.escribirArchivo("04GB030_2.gv");
+//	GraphM cuatroBFS = cuatro.BFS(0);
+//	cuatroBFS.escribirArchivo("04GB030_2BFS.gv");
+//	GraphM cuatroDFS = cuatro.DFS(0);
+//	cuatroDFS.escribirArchivo("04GB030_2DFS.gv");
+//	GraphM cuatroDFS_I = cuatro.DFS_I(0);
+//	cuatroDFS_I.escribirArchivo("04GB030_2DFS_I.gv");
+	
+//	GraphM cinco = new GraphM(100);
+//	cinco.modeloGilbert(0.9,false);
+//	cinco.escribirArchivo("05GB100_2.gv");
+//	GraphM cincoBFS = cinco.BFS(0);
+//	cincoBFS.escribirArchivo("05GB100_2BFS.gv");
+//	GraphM cincoDFS = cinco.DFS(0);
+//	cincoDFS.escribirArchivo("05GB100_2DFS.gv");
+//	GraphM cincoDFS_I = cinco.DFS_I(0);
+//	cincoDFS_I.escribirArchivo("05GB100_2DFS_I.gv");
+	
+//	GraphM seis = new GraphM(500);
+//	seis.modeloGilbert(0.9,false);
+//	seis.escribirArchivo("06GB500_2.gv");
+//	GraphM seisBFS = seis.BFS(0);
+//	seisBFS.escribirArchivo("06GB500_2BFS.gv");
+//	GraphM seisDFS = seis.DFS(0);
+//	seisDFS.escribirArchivo("06GB500_2DFS.gv");
+//	GraphM seisDFS_I = seis.DFS_I(0);
+//	seisDFS_I.escribirArchivo("06GB500_2DFS_I.gv");
+
+//	GraphM siete = new GraphM(30);
+//	siete.modeloGeoSimple(0.9,false);
+//	siete.escribirArchivo("07GS030_2.gv");
+//	GraphM sieteBFS = siete.BFS(0);
+//	sieteBFS.escribirArchivo("07GS030_2BFS.gv");
+//	GraphM sieteDFS = siete.DFS(0);
+//	sieteDFS.escribirArchivo("07GS030_2DFS.gv");
+//	GraphM sieteDFS_I = siete.DFS_I(0);
+//	sieteDFS_I.escribirArchivo("07GS030_2DFS_I.gv");
+	
+//	GraphM ocho = new GraphM(100);
+//	ocho.modeloGeoSimple(0.9,false);
+//	ocho.escribirArchivo("08GS100_2.gv");
+//	GraphM ochoBFS = ocho.BFS(0);
+//	ochoBFS.escribirArchivo("08GS100_2BFS.gv");
+//	GraphM ochoDFS = ocho.DFS(0);
+//	ochoDFS.escribirArchivo("08GS100_2DFS.gv");
+//	GraphM ochoDFS_I = ocho.DFS_I(0);
+//	ochoDFS_I.escribirArchivo("08GS100_2DFS_I.gv");
+	
+//	GraphM nueve = new GraphM(500);
+//	nueve.modeloGeoSimple(0.9,false);
+//	nueve.escribirArchivo("09GS500_2.gv");
+//	GraphM nueveBFS = nueve.BFS(0);
+//	nueveBFS.escribirArchivo("09GS500_2BFS.gv");
+//	GraphM nueveDFS = nueve.DFS(0);
+//	nueveDFS.escribirArchivo("09GS500_2DFS.gv");
+//	GraphM nueveDFS_I = nueve.DFS_I(0);
+//	nueveDFS_I.escribirArchivo("09GS500_2DFS_I.gv");
+	
+//	GraphM diez = new GraphM(30);
+//	diez.modeloBA(15);
+//	diez.escribirArchivo("10BA030_2.gv");
+//	GraphM diezBFS = diez.BFS(0);
+//	diezBFS.escribirArchivo("10BA030_2BFS.gv");
+//	GraphM diezDFS = diez.DFS(0);
+//	diezDFS.escribirArchivo("10BA030_2DFS.gv");
+//	GraphM diezDFS_I = diez.DFS_I(0);
+//	diezDFS_I.escribirArchivo("10BA030_2DFS_I.gv");
+	
+//	GraphM once = new GraphM(100);
+//	once.modeloBA(50);
+//	once.escribirArchivo("11BA100_2.gv");
+//	GraphM onceBFS = once.BFS(0);
+//	onceBFS.escribirArchivo("11BA100_2BFS.gv");
+//	GraphM onceDFS = once.DFS(0);
+//	onceDFS.escribirArchivo("11BA100_2DFS.gv");
+//	GraphM onceDFS_I = once.DFS_I(0);
+//	onceDFS_I.escribirArchivo("11BA100_2DFS_I.gv");
+	
+	GraphM doce = new GraphM(500);
+	doce.modeloBA(250);
+//	doce.escribirArchivo("12BA500_2.gv");
+	GraphM doceBFS = doce.BFS(0);
+	doceBFS.escribirArchivo("12BA500_2BFS.gv");
+	GraphM doceDFS = doce.DFS(0);
+	doceDFS.escribirArchivo("12BA500_2DFS.gv");
+	GraphM doceDFS_I = doce.DFS_I(0);
+	doceDFS_I.escribirArchivo("12BA500_2DFS_I.gv");
+	}
+
+}
